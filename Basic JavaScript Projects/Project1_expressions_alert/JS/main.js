e96537d6921ed014b52910108fcff92185e39541
ponczokjozsef-create <ponczokjozsef@gmail.com>
2026-09-14 17:21:07 -0400

    Add escape character example to main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,3 +2,5 @@ window.alert("Hello! This is my first JavaScript alert.");
 document.write("Hello! This is written directly on the page.");
 let myMessage = "This is my string value!";
 window.alert(myMessage);
+let quoteExample = "She said, \"JavaScript is fun!\" and I couldn't agree more.";
+window.alert(quoteExample);
