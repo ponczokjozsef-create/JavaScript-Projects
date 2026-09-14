4ad9f328210aaf6b630d87f3e6a65edd56932b4a
ponczokjozsef-create <ponczokjozsef@gmail.com>
2026-09-14 17:16:12 -0400

    Add string variable and display with window.alert()

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,4 @@
 window.alert("Hello! This is my first JavaScript alert.");
 document.write("Hello! This is written directly on the page.");
+let myMessage = "This is my string value!";
+window.alert(myMessage);
