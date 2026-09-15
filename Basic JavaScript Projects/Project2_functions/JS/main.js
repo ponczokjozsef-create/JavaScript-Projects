04e2ad58925e916253af12599d84e569c2856ba6
ponczokjozsef-create <ponczokjozsef@gmail.com>
2026-09-15 14:42:57 -0400

    Add += operator example with document.getElementById

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,3 +1,8 @@
+// Uses the += operator to concatenate a string, then displays it using document.getElementById
+let baseText = "JavaScript ";
+baseText += "is a powerful language.";
+document.getElementById("operatorOutput").innerHTML = baseText;
+
 // Function that updates the paragraph text using two variables and document.getElementById
 function updateMessage() {
   let greeting = "Hello there!";
