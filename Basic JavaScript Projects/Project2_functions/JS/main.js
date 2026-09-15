9ae75bf5651d87d25d5abd1dfd5884580d50bb8f
ponczokjozsef-create <ponczokjozsef@gmail.com>
2026-09-15 14:35:04 -0400

    Add Project2 functions with document.getElementById

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+// Function that updates the paragraph text using two variables and document.getElementById
+function updateMessage() {
+  let greeting = "Hello there!";
+  let name = "Joseph";
+  document.getElementById("output").innerHTML = greeting + " " + name;
+}
