f386d022a04108ae07547a63dc3ff4f84fc8aaf7
ponczokjozsef-create <ponczokjozsef@gmail.com>
2026-09-15 14:16:51 -0400

    Add Sent1/Sent2 concatenation and comments to main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,10 +1,25 @@
+// Displays a simple alert message to the user
 window.alert("Hello! This is my first JavaScript alert.");
+
+// Writes a message directly onto the webpage
 document.write("Hello! This is written directly on the page.");
+
+// Declares a string variable and displays it in an alert
 let myMessage = "This is my string value!";
 window.alert(myMessage);
+
+// Demonstrates escaping quotation marks and an apostrophe within a string
 let quoteExample = "She said, \"JavaScript is fun!\" and I couldn't agree more.";
 window.alert(quoteExample);
+
+// Declares a variable and concatenates it into a string using the + operator
 let firstName = "Joseph";
 document.write("Hello, " + firstName + "! Welcome to JavaScript.");
+
+// Evaluates an arithmetic expression and stores the result in a variable
 let result = 5 + 3 * 2;
 document.write("<br>The result of the expression 5 + 3 * 2 is: " + result);
+// Declares two variables with specific string values and concatenates them
+let Sent1 = "This is the beginning of the string";
+let Sent2 = " and this is the end of the string";
+document.write("<br>" + Sent1 + Sent2);
